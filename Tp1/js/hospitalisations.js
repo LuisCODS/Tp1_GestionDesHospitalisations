0f6b4a57348a63a8ed8795c3444fe005080a72ad
LuisCODS <tennis.bresil@gmail.com>
2019-01-15 20:27:51 -0500

    1/2
    Finalied

--- Tp1/js/hospitalisations.js
+++ Tp1/js/hospitalisations.js
@@ -2,16 +2,16 @@
 // <-- ============================== PATIENTS ============================== -->
 
 //OBJETS...
-var Patient1  = {Dossier: 1,   Nom:"Leger",     Prenom:"Emile", 	Naissence: "26 mars 1917",     Sexe:"M"};
-var Patient2  = {Dossier: 2,   Nom:"Bernard",   Prenom:"Marie", 	Naissence: "3 fevrier 1946",   Sexe:"F"};
-var Patient3  = {Dossier: 3,   Nom:"Chartrand", Prenom:"Guy", 	    Naissence: "5 avril 1959",     Sexe:"M"};
-var Patient4  =	{Dossier: 4,   Nom:"Cote",    	Prenom:"Andre", 	Naissence: "2 septembre 1978", Sexe:"M"};
-var Patient5  =	{Dossier: 5,   Nom:"Lavoie",    Prenom:"Carole", 	Naissence: "4 novembre 1973",  Sexe:"F"};
-var Patient6  =	{Dossier: 6,   Nom:"Martin",    Prenom:"Diane", 	Naissence: "2 decembre 1965",  Sexe:"F"};
-var Patient7  =	{Dossier: 7,   Nom:"Lacroix",   Prenom:"Pauline",   Naissence: "25 janvier 1956",  Sexe:"F"};
-var Patient8  =	{Dossier: 8,   Nom:"St-Jean",   Prenom:"Arthur", 	Naissence: "7 octobre 1912",   Sexe:"M"};
-var Patient9  =	{Dossier: 9,   Nom:"Bechard",   Prenom:"Marc", 	    Naissence: "8 aout 1980",      Sexe:"M"};
-var Patient10 =	{Dossier: 10,  Nom:"Chartrand", Prenom:"Mario",     Naissence: "23 juillet 1947",  Sexe:"M"};
+var Patient1  = {Dossier: 01,   Nom:"Leger",     Prenom:"Emile", 	Naissence: "26 mars 1917",     Sexe:"M"};
+var Patient2  = {Dossier: 02,   Nom:"Bernard",   Prenom:"Marie", 	Naissence: "3 fevrier 1946",   Sexe:"F"};
+var Patient3  = {Dossier: 03,   Nom:"Chartrand", Prenom:"Guy", 	    Naissence: "5 avril 1959",     Sexe:"M"};
+var Patient4  =	{Dossier: 04,   Nom:"Cote",      Prenom:"Andre", 	Naissence: "2 septembre 1978", Sexe:"M"};
+var Patient5  =	{Dossier: 05,   Nom:"Lavoie",    Prenom:"Carole", 	Naissence: "4 novembre 1973",  Sexe:"F"};
+var Patient6  =	{Dossier: 06,   Nom:"Martin",    Prenom:"Diane", 	Naissence: "2 decembre 1965",  Sexe:"F"};
+var Patient7  =	{Dossier: 07,   Nom:"Lacroix",   Prenom:"Pauline",  Naissence: "25 janvier 1956",  Sexe:"F"};
+var Patient8  =	{Dossier: 08,   Nom:"St-Jean",   Prenom:"Arthur", 	Naissence: "7 octobre 1912",   Sexe:"M"};
+var Patient9  =	{Dossier: 09,   Nom:"Bechard",   Prenom:"Marc", 	Naissence: "8 aout 1980",      Sexe:"M"};
+var Patient10 =	{Dossier: 10,   Nom:"Chartrand", Prenom:"Mario",    Naissence: "23 juillet 1947",  Sexe:"M"};
 
 //TABLEAU 
 var tbl_Patients = [Patient1,
@@ -28,12 +28,24 @@ var tbl_Patients = [Patient1,
 // <-- =============================== ETABLISSEMENTS ============================== -->
 
 //OBJETS...
-var Etablissement1={Etablissement:1234,Nom:"Centre hospitalier Sud",Adresse:"1234 boul. Sud, Montreal, Qc",CodePostal:"H2M 3Y6",Telephone:"514-323-1234"};
-var Etablissement2={Etablissement:2346,Nom:"Hopital Nord",Adresse:"7562 rue Souvenir, Noreville, Qc",CodePostal:"J4R 2Z5",Telephone:"450-222-3333"};
-var Etablissement3={Etablissement:3980,Nom:"Hopital centre",Adresse:"4637, boul. de l'Eglise, Montreal, Qc",CodePostal:"H3J 4K8",Telephone:"514-123-5678"};
-var Etablissement4={Etablissement:4177,Nom:"Centre hospitalier est",Adresse:"12 rue Bernard, repentigny, Qc",CodePostal:"JBR 3K5",Telephone:"450-589-2345"};
-var Etablissement5={Etablissement:7306,Nom:"Hopital du salut",Adresse:"11 rue de la Redemtion, St-Basilie, Qc",CodePostal:"JBH 2D4",Telephone:"450-345-6789"};
-var Etablissement6={Etablissement:8895,Nom:"Dernier recours",Adresse:"999 rue St-Pierre, Longueuil, Qc",CodePostal:"J7B 3J5",Telephone:"450-555-6741"};
+
+var Etablissement1={
+    Etablissement:1234,Nom:"Centre hospitalier Sud",Adresse:"1234 boul. Sud, Montreal, Qc",CodePostal:"H2M 3Y6",Telephone:"514-323-1234"};
+
+var Etablissement2={
+    Etablissement:2346,Nom:"Hopital Nord",Adresse:"7562 rue Souvenir, Noreville, Qc",CodePostal:"J4R 2Z5",Telephone:"450-222-3333"};
+
+var Etablissement3={
+    Etablissement:3980,Nom:"Hopital centre",Adresse:"4637, boul. de l'Eglise, Montreal, Qc",CodePostal:"H3J 4K8",Telephone:"514-123-5678"};
+
+var Etablissement4={
+    Etablissement:4177,Nom:"Centre hospitalier est",Adresse:"12 rue Bernard, repentigny, Qc",CodePostal:"JBR 3K5",Telephone:"450-589-2345"};
+
+var Etablissement5={
+    Etablissement:7306,Nom:"Hopital du salut",Adresse:"11 rue de la Redemtion, St-Basilie, Qc",CodePostal:"JBH 2D4",Telephone:"450-345-6789"};
+
+var Etablissement6={
+    Etablissement:8895,Nom:"Dernier recours",Adresse:"999 rue St-Pierre, Longueuil, Qc",CodePostal:"J7B 3J5",Telephone:"450-555-6741"};
 
 //TABLEAU
 var tbl_Etablissements =[Etablissement1,
@@ -46,19 +58,19 @@ var tbl_Etablissements =[Etablissement1,
 // <-- =============================== HOSPITALISATIONS ============================== -->
 
 //OBJETS...
-var Hospitalisation1={CodeEtablissement:1234,NoDossierPatient:5,DateAdmission:"14-aout-00",DateSortie:"14-aout-01",Specialite:"medicine"};
-var Hospitalisation2={CodeEtablissement:1234,NoDossierPatient:10,DateAdmission:"12-dec.92",DateSortie:"12-dec.92",Specialite:"chirurgie"};
-var Hospitalisation3={CodeEtablissement:2346,NoDossierPatient:8,DateAdmission:"03-mars-03",DateSortie:"05-mars-03",Specialite:"medicine"};
-var Hospitalisation4={CodeEtablissement:2346,NoDossierPatient:1,DateAdmission:"11-nov.-97",DateSortie:"12-nov.-97",Specialite:"orthopedie"};
-var Hospitalisation5={CodeEtablissement:2346,NoDossierPatient:3,DateAdmission:"12-avr.-95",DateSortie:"30-avr.-95",Specialite:"medicine"};
-var Hospitalisation6={CodeEtablissement:3980,NoDossierPatient:5,DateAdmission:"14-fev.-00",DateSortie:"14-mars-00",Specialite:"medicine"};
-var Hospitalisation7={CodeEtablissement:3980,NoDossierPatient:5,DateAdmission:"01-jan.-01",DateSortie:"01-fev.-01",Specialite:"medicine"};
-var Hospitalisation8={CodeEtablissement:3980,NoDossierPatient:9,DateAdmission:"03-avr.-95",DateSortie:"08-avr.-95",Specialite:"orthopedie"};
-var Hospitalisation9={CodeEtablissement:3980,NoDossierPatient:7,DateAdmission:"27-nov.-99",DateSortie:"04-dec.-99",Specialite:"chirurgie"};
+var Hospitalisation1={CodeEtablissement:1234, NoDossierPatient:05,DateAdmission:"14-aout-00",DateSortie:"14-aout-01",Specialite:"medicine"};
+var Hospitalisation2={CodeEtablissement:1234, NoDossierPatient:10,DateAdmission:"12-dec.92",DateSortie:"12-dec.92",Specialite:"chirurgie"};
+var Hospitalisation3={CodeEtablissement:2346, NoDossierPatient:08,DateAdmission:"03-mars-03",DateSortie:"05-mars-03",Specialite:"medicine"};
+var Hospitalisation4={CodeEtablissement:2346, NoDossierPatient:01,DateAdmission:"11-nov.-97",DateSortie:"12-nov.-97",Specialite:"orthopedie"};
+var Hospitalisation5={CodeEtablissement:2346, NoDossierPatient:03,DateAdmission:"12-avr.-95",DateSortie:"30-avr.-95",Specialite:"medicine"};
+var Hospitalisation6={CodeEtablissement:3980, NoDossierPatient:05,DateAdmission:"14-fev.-00",DateSortie:"14-mars-00",Specialite:"medicine"};
+var Hospitalisation7={CodeEtablissement:3980, NoDossierPatient:05,DateAdmission:"01-jan.-01",DateSortie:"01-fev.-01",Specialite:"medicine"};
+var Hospitalisation8={CodeEtablissement:3980, NoDossierPatient:09,DateAdmission:"03-avr.-95",DateSortie:"08-avr.-95",Specialite:"orthopedie"};
+var Hospitalisation9={CodeEtablissement:3980, NoDossierPatient:07,DateAdmission:"27-nov.-99",DateSortie:"04-dec.-99",Specialite:"chirurgie"};
 var Hospitalisation10={CodeEtablissement:3980,NoDossierPatient:10,DateAdmission:"28-avr.-97",DateSortie:"05-mai.-97",Specialite:"chirurgie"};
-var Hospitalisation11={CodeEtablissement:4177,NoDossierPatient:3,DateAdmission:"03-aout-01",DateSortie:"05-dec.-01",Specialite:"medicine"};
-var Hospitalisation12={CodeEtablissement:4177,NoDossierPatient:3,DateAdmission:"02-fev.-02",DateSortie:"23-fev.-02",Specialite:"orthopedie"};
-var Hospitalisation13={CodeEtablissement:7306,NoDossierPatient:2,DateAdmission:"23-mai-98",DateSortie:"27-mai-98",Specialite:"orthopedie"};
+var Hospitalisation11={CodeEtablissement:4177,NoDossierPatient:03,DateAdmission:"03-aout-01",DateSortie:"05-dec.-01",Specialite:"medicine"};
+var Hospitalisation12={CodeEtablissement:4177,NoDossierPatient:03,DateAdmission:"02-fev.-02",DateSortie:"23-fev.-02",Specialite:"orthopedie"};
+var Hospitalisation13={CodeEtablissement:7306,NoDossierPatient:02,DateAdmission:"23-mai-98",DateSortie:"27-mai-98",Specialite:"orthopedie"};
 
 //TABLEAU
 var tbl_Hospitalisations =[	Hospitalisation1,
@@ -82,7 +94,7 @@ var tbl_Hospitalisations =[	Hospitalisation1,
         tbl_Hopital["Hospitalisations"] = tbl_Hospitalisations;				
 							
 							
-// <-- //////////////////////////////////  (FUNCTIONS) ////////////////////////////////// -->
+// <-- =================================  (FUNCTIONS) ================================= -->
 
 function initialize()
 {
@@ -108,9 +120,9 @@ function initialize()
 function showPatients(){   
 	hiddenDiv(); //clean before
     var size = tbl_Patients.length;
-	var table="<table class="+"table"+">";
-   
-       table+="<tr>"+
+	//var table="<table>";
+   var table="<table class=table>";
+       table+="<tr class=success>"+
 					"<td>"+"<strong>Dossier</strong>"+"</td>"+
 					"<td>"+"<strong>Nom</strong>"+"</td>"+
 					"<td>"+"<strong>Prenom</strong>"+"</td>"+
@@ -141,9 +153,9 @@ function showEtablissements(){
     hiddenDiv();
     var size = tbl_Etablissements.length;
     
-   var table="<table class="+"table"+">";
-    //1 LIGNE and 5 COLONNES
-       table+="<tr>"+
+    var table="<table class=table>";
+    //1 LIGNE and 5 COLONNES...
+       table+="<tr class=success>"+
 	   			"<td>"+"<strong>Etablissement</strong>"+"</td>"+
 				"<td>"+"<strong>Nom</strong>"+"</td>"+
 				"<td>"+"<strong>Adresse</strong>"+"</td>"+
@@ -173,9 +185,9 @@ function showHospitalisations(){
     hiddenDiv();
     var size = tbl_Hospitalisations.length;
     
-   var table="<table class="+"table"+">";
+    var table="<table class=table>";
     //1 LIGNE and 5 COLONNES
-       table+="<tr>"+
+       table+="<tr class=success>"+
 				"<td>"+"<strong>Code etablissement</strong>"+"</td>"+
 				"<td>"+"<strong>No dossier patient</strong>"+"</td>"+
 				"<td>"+"<strong>Date admission</strong>"+"</td>"+
@@ -206,25 +218,28 @@ function showHospitalisations(){
 */
 function chargerDossier()
 {   
-	//pour chaque click...
+	//pour chaque click, on cache les elements
 	hiddenDiv()
+    //Rendre visible la premiere dropdowlist
 	document.getElementById("selectByDossier").style.visibility = "visible";
-	
+	//get the tag select
     var selecDossier = document.getElementById('selectByDossier');
-    selecDossier.options.length=0;//to clen	
+    //clean the select
+    selecDossier.options.length=0;
+    //Give the first element : a text to display
     selecDossier.options[selecDossier.options.length]=new Option("Choisir  un dossier...");
 
+    //Cree une array et y placer tous les numeros de dossier des patients...
     var tblSort = new Array();
-	var taille = 1;
-    
+	var taille = 1;    
 	for(var prop in tbl_Hopital["Hospitalisations"] )
 	{
 		tblSort[taille] = tbl_Hopital["Hospitalisations"][prop].NoDossierPatient;
 		taille++;
 	}	
-    
-	tblSort.sort()//On trie avant de filtrer
-    
+    //On trie avant de filtrer
+	tblSort.sort()
+    //On filtre les doublons
 	for(var i = 0; i < tblSort.length;i++)	{
 		//remove each element equal to the preceding 
 		if( tblSort[i] != tblSort[i+1] ){
@@ -238,9 +253,9 @@ function chargerDossier()
 //Affiche le tableau des patients
 function showTableHospByPatient(select){
 	
-	var table="<table border="+1+" ; class="+"table"+">";
+        var table="<table class=table>";
 		//1 LIGNE and 5 COLONNES
-		table+="<tr>"+
+		table+="<tr class=success>"+
 					"<td>"+"<strong>Code etablissement</strong>"+"</td>"+
 					"<td>"+"<strong>Dossier</strong>"+"</td>"+
 					"<td>"+"<strong>Admission</strong>"+"</td>"+
@@ -276,7 +291,7 @@ function showTableHospByPatient(select){
 	document.getElementById('divColAffiche').innerHTML=table;	
 }
 
-// <-- _______________________________________  ONGLET - HOSPITALISATION PAR ÉTABLISSEMENT ET SPECIALITÉ _______________________________________ -->
+// <-- _______________________________________  ONGLET - HOSPITALISATION PAR ÉTABLISSEMENT ET SPECIALITÉ ________________________________ -->
 
 /*Affiche un champ SELECT pour la liste des codes d’établissement .
  Les champs SELECT sont générés directement à partir des données des tableaux JSON*/
@@ -289,6 +304,7 @@ function chargerEtablissiment(){
 		
     var selecCode = document.getElementById('selectByEtablissement');
 		selecCode.options.length=0;//on vide	
+        selecCode.options[selecCode.options.length]=new Option("Choisir...");
 
     var tblSort = new Array();
 	var taille = 1;    
@@ -320,10 +336,10 @@ function chargerSpecialite(selecCode)
 	var obj ;	
 	var specialiteToCompare = "";
 		selSpecialite.options.length = 0;//vider le select 
+        selSpecialite.options[selSpecialite.options.length]=new Option("Choisir la spécialité...");
     var tblSort = new Array();
 	var taille = 1;
 
-	selSpecialite.options[selSpecialite.options.length] = new Option("Choisir...");
 	for(var prop in tbl_Hopital["Hospitalisations"] )
 	{				
 		obj = tbl_Hopital["Hospitalisations"][prop];//get objet of array		
@@ -356,9 +372,9 @@ function showTableBySpecialite(select)
 	var code = selCode.options[selCode.selectedIndex].text;
 		code = code.substring(0,4);
 		
-	var table="<table border="+1+" ; class="+"table"+">";
+	var table="<table class=table>";
 		//1 LIGNE and 5 COLONNES
-		table+="<tr>"+
+		table+="<tr class=success>"+
 					"<td>"+"<strong>Code</strong>"+"</td>"+
 					"<td>"+"<strong>Dossier</strong>"+"</td>"+
 					"<td>"+"<strong>Admission</strong>"+"</td>"+
